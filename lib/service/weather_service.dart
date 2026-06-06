46423631deb9b1ef8131da0a0c5f35ff616f2a56
naycadenfernandes-07 <fernandesnaycaden@gmail.com>
2026-06-06 22:14:18 +0530

    Add WeatherService class for fetching weather data and current city name

---
+++ lib/service/weather_service.dart
@@ -0,0 +1,43 @@
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+import '../models/weather_model.dart';
+import 'dart:convert';
+
+
+class WeatherService {
+
+  static const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
+  final String apiKey;
+
+  WeatherService({required this.apiKey});
+
+  Future<Weather> getWeather(String cityname) async{
+    final response = await http.get(Uri.parse('$BASE_URL?q=$cityname&appid=$apiKey&units=metric'));
+
+    if(response.statusCode == 200 ) {
+      return Weather.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
+    } else {
+      throw Exception('Failed to load weather data');
+    }
+  }
+
+  Future<String> getCurrentCityName() async {
+    LocationPermission permission= await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        throw Exception('Location permissions are denied');
+      }
+    }
+
+    Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high
+    );
+
+    List <Placemark> placemarks = await placemarkFromCoordinates(position.latitude, position.longitude);
+
+    String? city = placemarks[0].locality;
+    return city ?? '';
+  }
+}
