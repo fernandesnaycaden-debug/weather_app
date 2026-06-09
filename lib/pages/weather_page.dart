cef52ca3ab8bc967f741fcff62d5694e64711913
naycadenfernandes-07 <fernandesnaycaden@gmail.com>
2026-06-09 23:28:12 +0530

    Implement weather fetching functionality in WeatherPage

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/models/weather_model.dart';
+import 'package:weather_app/service/weather_service.dart';
 
 class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
@@ -8,14 +10,51 @@ class WeatherPage extends StatefulWidget {
 }
 
 class _WeatherPageState extends State<WeatherPage> {
+  
+  //api key
+  final _weatherService = WeatherService(apiKey: 'api_Key');
+  Weather? _weather;
+
+  fetchWeather() async {
+
+    String cityname = await _weatherService.getCurrentCityName();
+
+  try {
+    final weather = await _weatherService.getWeather(cityname);
+    setState(() {
+      _weather = weather;
+    });
+  }
+
+  catch (e) {
+    print(e);
+  }
+  }
+
+  @override
+
+  void initState(){
+    super.initState();
+    fetchWeather();
+  }
+  
+  
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Weather App'),
       ),
-      body: const Center(
-        child: Text('Weather information will be displayed here.'),
+      body: Center(
+        child: Column (
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(_weather?.cityName ?? 'Loading...'),
+        
+            Text('${_weather?.temperature.round()}°C') 
+          ]
+        ),
       ),
     );
   }
