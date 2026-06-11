f389c4adf366d8a1cfc91dafabf98643f8528cbe
naycadenfernandes-07 <fernandesnaycaden@gmail.com>
2026-06-11 21:18:41 +0530

    Refactor Weather model to include dt, sunrise, and sunset fields

--- lib/models/weather_model.dart
+++ lib/models/weather_model.dart
@@ -2,18 +2,32 @@ class Weather {
   final String cityName;
   final double temperature;
   final String mainCondition;
+  final int dt;
+  final int sunrise;
+  final int sunset;
 
   Weather({
     required this.cityName,
     required this.temperature,
     required this.mainCondition,
+    required this.dt,
+    required this.sunrise,
+    required this.sunset,
   });
+
+//To check whether it;s night time at the current location 
+  bool get isNight {
+    return dt <sunrise || dt > sunset;
+  }
 
   factory Weather.fromJson(Map<String, dynamic> json) {
     return Weather(
       cityName: json['name'],
       temperature: json['main']['temp'].toDouble(),
       mainCondition: json['weather'][0]['main'],
+      dt: json['dt'],
+      sunrise: json['sys']['sunrise'],
+      sunset: json['sys']['sunset'],
     );
   }
 }
